232e5beacc5be4d4534ce68aa9392c3b44ed64b9
Parth Tiwari <youremail@gmail.com>
2025-07-10 21:53:29 +0530

    Fix: Move script tag using next/script in layout.js

--- app/layout.js
+++ app/layout.js
@@ -1,6 +1,7 @@
 import './globals.css'
 import Navbar from '../components/Navbar'
 import Footer from '../components/Footer'
+import Script from 'next/script'
 
 export const metadata = {
   title: 'Table Booking App',
@@ -10,24 +11,14 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <head>
-        <script
+      <body className="relative bg-gray-50 text-gray-900 overflow-x-hidden">
+        <Script
           src="https://unpkg.com/@dotlottie/player-component@2.7.12/dist/dotlottie-player.mjs"
           type="module"
-          async
-        ></script>
-
-      </head>
-
-      <body className="relative bg-gray-50 text-gray-900 overflow-x-hidden">
-
-
-
-
+          strategy="afterInteractive"
+        />
         <Navbar />
         <main>{children}</main>
-
-
         <Footer />
       </body>
     </html>
